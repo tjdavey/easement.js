d7e6d2c15c601f9700456aa0ece97980e26442f7
Tristan Davey <tristan@tristandavey.com>
2015-10-15 13:06:16 +1000

    Increase coverage

--- test/easement.js
+++ test/easement.js
@@ -40,14 +40,36 @@ describe('Easement.js', function() {
       }).to.throw(Error);
     });
 
+    it('should return startValue when current time is less than startTime', function(){
+      var result = Easement.ease(linearEasing, 50, {startTime: 100, startValue: 200});
+      expect(result).to.equal(200);
+    });
+
+    it('should return endValue when current time is less than endTime', function(){
+      var result = Easement.ease(linearEasing, 150, {endTime: 100, endValue: 200});
+      expect(result).to.equal(200);
+    });
+
   });
 
   describe('easings', function() {
-    for(easingString in Easement.easings){
-      var easingFunction = Easement.easings[easingString];
+    for (easingString in Easement.easings){
       describe(easingString+'()', function(){
+
+        var easingFunction = Easement.easings[easingString];
+
         it('should return an numeric value between 0 and 1', function(){
           var result = easingFunction(0.5);
+          expect(result).to.within(0, 1);
+        });
+
+        it('should return 0 at minimum time', function(){
+          var result = easingFunction(0);
+          expect(result).to.within(0, 1);
+        });
+
+        it('should return 1 at maximum time', function(){
+          var result = easingFunction(1);
           expect(result).to.within(0, 1);
         });
       });
